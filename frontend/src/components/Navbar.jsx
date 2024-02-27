d80fe02dc98e25b03be0587e1ae61db7fb0df6d2
junaidshaikh2004 <158518464+junaidshaikh2004@users.noreply.github.com>
2024-02-28 00:01:41 +0530

    navbar (#6)
    * navbar
    
    * navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,26 @@ import "../styles/navbar.css"
 function Navbar() {
     // check figma file for navbar
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div id="nav_left">
+        <div id="logo">
+          <img src="./images/logo.jpeg" alt="" />
+        </div>
+        <div id="name">
+          <h5 id='main'>rcoe</h5>
+          <h4 id='second'>code cell</h4>
+        </div>
+      </div>
+      <div id="nav_right">
+        <a href=""><img src="./images/join.png" alt="" /></a>
+        <a href="">Home</a>
+        <a href="">Events</a>
+        <a href="">Gallery</a>
+        <a href="">Blogs</a>
+        <a href="">Members</a>
+
+      </div>
+    </div>
   )
 }
 

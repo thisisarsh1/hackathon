30fe7172a5fdbbcb5f3b86c68fa76e098366f7c6
smitjiwani <smitjiwani44@gmail.com>
2024-02-29 23:35:23 +0530

    Basic implentation of framer motion 👍👍

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 // import "../styles/hero.css"
 import "/src/styles/Hero.css"
+import { motion, useScroll } from "framer-motion"
 
 function Hero() {
     // check figma file for hero
   return (
-    <div className='hero'>Hero</div>
+    <div className='hero'>
+      <motion.div 
+      initial={{ opacity: 0, marginLeft: -200 }}
+      whileInView={{ opacity: 1, marginLeft: 0 }}
+      transition={{ duration: 1 }}
+      className="box"
+      >
+          <p>this is a box</p>
+      </motion.div>
+    </div>
   )
 }
 

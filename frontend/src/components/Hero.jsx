18f73f8ec2d3b2eb9e4a81f9f1494e0a84cb923f
junaidshaikh2004 <158518464+junaidshaikh2004@users.noreply.github.com>
2024-03-03 23:36:29 +0530

    Hero (#14)
    * navbar
    
    * navbar
    
    ---------
    
    Co-authored-by: Smit Jiwani <78549024+smitjiwani@users.noreply.github.com>

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 // import "../styles/hero.css"
 import "/src/styles/Hero.css"
+import img3 from "/src/assets/Group1.png"
+import vector2 from "/src/assets/vector2.png"
+
 import { motion, useScroll } from "framer-motion"
 import { Button } from 'flowbite-react';
 
@@ -8,9 +11,27 @@ function Hero() {
     // check figma file for hero
   return (
     <div className='hero'>
-   
+
+      <img className="background" src={vector2} alt="" />
+      <div id="hero_left">
+        <div id="text">
+          <h3 id="main" >RCOE</h3>
+          <h3 id="second" >CODE</h3>
+          <h3 id="second" >CELL</h3>
+        </div>
+        <div id="head">
+        <img src={img3}></img>
+        <div className="text">Lorem</div>
+        </div>
+        
+      </div>
+     
+      <div id="gif">
+        <img src="./robot1.gif" alt="" />
+      </div>
     </div>
-    
+
+
   )
 }
 
